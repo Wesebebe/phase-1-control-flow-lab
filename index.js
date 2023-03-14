e55a390e20043d32ca2c33563e6f9c1ddc99111a
+ <+>
2023-03-14 11:24:14 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return "This one is on me!";
+  }if(ride > 2000 && ride < 2500){
+    return "I will gladly take your thirty bucks.";
+  }else if(ride > 2500){
+    return "No can do.";
+  }
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = city === "NYC"? 'Ok, sounds good.' : 'No go.';
+  return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(trip){
+  switch(trip){
+    case "generous" :
+      return "Thank you so much.";
+    case 'not as generous' :
+      return 'Thank you.';
+    default:
+      return "Bye.";
+  }
 }
